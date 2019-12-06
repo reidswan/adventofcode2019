b22d9effee0f605a068128003aff423da5998f50
reidswan <reidswan@outlook.com>
2019-12-06 07:04:29 +0200

    Gotta go fast

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,83 +1,60 @@
 pub fn part1(src: &(u32, u32)) {
     let (start, end) = src;
-    println!("Part 1 = {}", count_with_test(*start, *end+1, test_part1));
+    println!("Part 1 = {}", (*start..*end+1).filter(|&i|test_part1(i)).count());
 }
 
 pub fn part2(src: &(u32, u32)) {
     let (start, end) = src;
-    println!("Part 2 = {}", count_with_test(*start, *end+1, test_part2));
+    println!("Part 1 = {}", (*start..*end+1).filter(|&i|test_part2(i)).count());
 }
 
-fn count_with_test(start: u32, end: u32, test: fn(u32)-> bool)-> usize {
-    (start..end).filter(|i| test(*i)).count()
-}
+fn test_part1(d: u32)-> bool {
+    if d < 111111 || d > 999999 {
+        return false
+    }
 
-fn digitize(source: u32)-> Vec<u8> {
-    let mut num = source;
-    let mut digits = vec![];
+    let mut num = d/10;
+    let mut prev_digit = d % 10;
+    let mut has_repeat = false;
     while num > 0 {
-        let digit = (num % 10) as u8;
-        num /= 10;
-        digits.push(digit)
-    };
-    digits.reverse();
-    digits
-}
-
-fn group_digits(digits: &Vec<u8>)-> Vec<Vec<u8>> {
-    let mut groups = vec![];
-    let mut prev_digit = &digits[0];
-    let mut group = vec![*prev_digit];
-    
-    for digit in digits.iter().skip(1) {
-        if digit == prev_digit {
-            group.push(*digit)
-        } else {
-            groups.push(group);
-            group = vec![*digit];
-        };
-        prev_digit = digit;
-    }
-
-    groups.push(group);
-    groups
-}
-
-fn non_decreasing(digits: &Vec<u8>)-> bool {
-    if digits.len() == 0 {
-        true
-    } else {
-        let mut prev_digit = &digits[0];
-        for digit in digits.iter().skip(1) {
-            if prev_digit > digit {
-                return false
-            }
-            prev_digit = digit
+        let current_digit = num % 10;
+        if current_digit > prev_digit {
+            return false
+        } else if current_digit == prev_digit {
+            has_repeat = true
         }
-        true
+        prev_digit = current_digit;
+        num /= 10;
     }
+    has_repeat
 }
 
-fn has_any_repeat(groups: &Vec<Vec<u8>>)-> bool {
-    groups.iter().any(|grp| grp.len() >= 2)
-}
+fn test_part2(d: u32)-> bool {
+    if d < 111111 || d > 999999 {
+        return false
+    }
 
-fn has_strict_repeat(groups: &Vec<Vec<u8>>)-> bool {
-    groups.iter().any(|grp| grp.len() == 2)
-}
+    let mut has_strict_repeat = false;
+    let mut current_repeat_length = 1;
+    let mut prev_digit = d % 10;
+    let mut num = d/10;
+    while num > 0 {
+        let current_digit = num % 10;
+        if current_digit > prev_digit {
+            return false
+        } else if current_digit == prev_digit {
+            current_repeat_length += 1
+        } else {
+            if current_repeat_length == 2 {
+                has_strict_repeat = true
+            }
+            current_repeat_length = 1;
+        }
+        prev_digit = current_digit;
+        num /= 10
+    }
 
-fn test_part1(i: u32)-> bool {
-    let digits = digitize(i);
-    let groups = group_digits(&digits);
-
-    digits.len() == 6 && non_decreasing(&digits) && has_any_repeat(&groups)
-}
-
-fn test_part2(i: u32)-> bool {
-    let digits = digitize(i);
-    let groups = group_digits(&digits);
-
-    digits.len() == 6 && non_decreasing(&digits) && has_strict_repeat(&groups)
+    has_strict_repeat || current_repeat_length == 2
 }
 
 pub fn get_parsed_input()-> (u32, u32) {
