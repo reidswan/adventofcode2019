09948c6a988aac11e6616a7d70e4e087787aa1d5
Reid Swan <reidswan@outlook.com>
2019-12-14 14:29:58 +0200

    Finalize the Int Code Machine for Day 9

---
+++ day9/src/lib.rs
@@ -0,0 +1,19 @@
+use common::int_code_machine::Machine;
+
+pub fn get_parsed_input()-> Machine {
+    Machine::new(include_str!("input/input"), vec![])
+}
+
+pub fn part1(input: &Machine) {
+    let mut machine = input.clone();
+    machine.add_input(1);
+    machine.run();
+    println!("Part 1 = {:?}", machine.output.last().unwrap());
+}
+
+pub fn part2(input: &Machine) {
+    let mut machine = input.clone();
+    machine.add_input(2);
+    machine.run();
+    println!("Part 2 = {:?}", machine.output.last().unwrap());
+}
